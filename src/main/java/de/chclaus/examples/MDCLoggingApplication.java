b378e40383fb0cb660a45de9f79c98006630462a
Christian Claus <ch.claus@me.com>
2016-02-11 22:47:33 +0100

    removed user services

--- src/main/java/de/chclaus/examples/MDCLoggingApplication.java
+++ src/main/java/de/chclaus/examples/MDCLoggingApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RequestLoggingApplication {
+public class MDCLoggingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RequestLoggingApplication.class, args);
+		SpringApplication.run(MDCLoggingApplication.class, args);
 	}
 }
