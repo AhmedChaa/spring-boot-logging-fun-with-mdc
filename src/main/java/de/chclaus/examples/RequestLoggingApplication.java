e1d8d4620c53115e735ded61509cdf73099958cd
Christian Claus <ch.claus@me.com>
2016-02-11 21:53:27 +0100

    skeleton

---
+++ src/main/java/de/chclaus/examples/RequestLoggingApplication.java
@@ -0,0 +1,12 @@
+package de.chclaus.examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestLoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequestLoggingApplication.class, args);
+	}
+}
